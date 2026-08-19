056a4e766de631272e3bf0cb9450934781614790
VeylornX 🕷️ <veylornxsupport@gmail.com>
2026-08-20 00:34:32 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("IG Welcome Bot is running!");
 });
+app.get("/webhook", (req, res) => {
+  res.status(200).send("Webhook is working!");
+});
 
+app.post("/webhook", (req, res) => {
+  console.log("Webhook received:", req.body);
+
+  res.sendStatus(200);
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
